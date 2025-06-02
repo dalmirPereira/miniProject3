f1f88b12f6f7fe3b67b5862ca81030e89a9b4a97
lionardus87 <yudhi_lee@hotmail.com>
2025-06-02 19:49:47 +1000

    update book database and using book list from database

--- client/src/contexts/BookContext.jsx
+++ client/src/contexts/BookContext.jsx
@@ -28,6 +28,8 @@ export const BookProvider = ({ children }) => {
 				"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS68RY84hFCFEv79YFlT9_dhetWuxhIO9Un6w&s",
 		},
 	]);
+	// const [loading, setLoading] = useState(true);
+	// const [error, setError] = useState(null);
 
 	const deleteBook = (id) => {
 		setBooks((prev) => prev.filter((book) => book.id !== id));
@@ -38,6 +40,63 @@ export const BookProvider = ({ children }) => {
 			prev.map((book) => (book.id === updatedBook.id ? updatedBook : book))
 		);
 	};
+
+	// useEffect(() => {
+	// 	const fetchBooks = async () => {
+	// 		try {
+	// 			const res = await fetch("http://localhost:3000/api/books");
+	// 			if (!res.ok) throw new Error("Failed to fetch books");
+
+	// 			const data = await res.json();
+	// 			setBooks(data);
+	// 		} catch (err) {
+	// 			console.error("Error fetching books:", err);
+	// 			setError(err.message);
+	// 		} finally {
+	// 			setLoading(false);
+	// 		}
+	// 	};
+
+	// 	fetchBooks();
+	// }, []);
+
+	// const deleteBook = async (id) => {
+	// 	try {
+	// 		const res = await fetch(`http://localhost:3000/api/books/${id}`, {
+	// 			method: "DELETE",
+	// 		});
+	// 		if (!res.ok) throw new Error("Delete failed");
+
+	// 		setBooks((prev) => prev.filter((book) => book.id !== id));
+	// 	} catch (err) {
+	// 		console.error("Delete error:", err);
+	// 		alert("Failed to delete book.");
+	// 	}
+	// };
+
+	// const updateBook = async (updatedBook) => {
+	// 	try {
+	// 		const res = await fetch(
+	// 			`http://localhost:3000/api/books/${updatedBook.id}`,
+	// 			{
+	// 				method: "PUT",
+	// 				headers: {
+	// 					"Content-Type": "application/json",
+	// 				},
+	// 				body: JSON.stringify(updatedBook),
+	// 			}
+	// 		);
+	// 		if (!res.ok) throw new Error("Update failed");
+
+	// 		const newBook = await res.json();
+	// 		setBooks((prev) =>
+	// 			prev.map((book) => (book.id === newBook.id ? newBook : book))
+	// 		);
+	// 	} catch (err) {
+	// 		console.error("Update error:", err);
+	// 		alert("Failed to update book.");
+	// 	}
+	// };
 
 	return (
 		<BookContext.Provider value={{ books, setBooks, deleteBook, updateBook }}>
